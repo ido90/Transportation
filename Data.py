11d32cd666d6d4e44591bac7299e46cdbe24598f
Oded Shimon <kmenc15@gmail.com>
2019-03-29 11:35:12 +0300

    Paths change

--- Data.py
+++ Data.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
 
-def load_drives(path=r'D:\Code\Python\Transportation\Data\train.csv'):
+def load_drives(path=r'data/train.csv'):
     if not path.endswith('.csv'): path += '.csv'
     return pd.read_csv(path)
 
-def load_lines(path=r'D:\Code\Python\Transportation\Data\shapes.csv'):
+def load_lines(path=r'data/shapes.csv'):
     if not path.endswith('.csv'): path += '.csv'
     return pd.read_csv(path)
 
