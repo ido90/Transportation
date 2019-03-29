0cd40928b8d361388f3fd5fff7899a31214a847a
Ido Greenberg <idogreenberg90@gmail.com>
2019-03-29 23:42:26 +0300

    general order

--- Data.py
+++ Data.py
@@ -10,27 +10,55 @@ class Drive:
         self.id = id
         self.points = points
 
-def to_meters(lat,lon):
-    # Approximations:
-    # earth radius ~ 6400km
-    # israel lat ~ 32 degrees
-    # cos(32 degrees) ~ 0.84
-    return ((lat - 32) / 180 * np.pi * 6400000,
-            (lon - 34) / 180 * np.pi * 6400000 * 0.84)
+def load_drives(path=r'data/train.csv', required_points=25,
+                permit_unlabeled=False, plot=True):
 
-def load_drives(path=r'data/train.csv', required_points=20):
+    # load from file
     if not path.endswith('.csv'): path += '.csv'
     drives = pd.read_csv(path)
-    if 'route_id' not in drives.columns:
+
+    if permit_unlabeled and 'route_id' not in drives.columns:
         drives['route_id'] = 0
 
+    # convert
     data = zip(drives.trip_index, drives.route_id, drives.lat, drives.lon)
-    all_drives = [ Drive(k, [ to_meters(lat,lon) for id1,id2,lat,lon in giter ])
-                   for k,giter in itertools.groupby(data, key=lambda x:"{} {}".format(x[0],x[1])) ]
+    all_drives = [Drive(k, [to_meters(lat, lon) for id1, id2, lat, lon in giter
+                            if lat and lon])
+                  for k, giter in itertools.groupby(data,
+                                                    key=lambda x:"{} {}".format(x[0],x[1]))]
+
+    # validate tracks lengths
+    if plot:
+        plot_drives(all_drives, required_points)
     if required_points > 0:
         all_drives = [d for d in all_drives if len(d.points) >= required_points]
 
     return all_drives
+
+def plot_drives(all_drives, required_points):
+    invalid = np.sum([len(d.points) < required_points for d in all_drives])
+    f, axs = plt.subplots(1, 1)
+    if required_points:
+        plt.plot([0, 100], [required_points, required_points],
+                 'r-', label='threshold')
+    x = 100 * np.array(list(range(len(all_drives)))) / len(all_drives)
+    axs.plot(x, sorted([len(d.points) for d in all_drives]), 'k-')
+    axs.set_xlim([0, 100])
+    axs.set_xlabel('Drive Percentile [%]', fontsize=12)
+    axs.set_ylabel('Number of GPS observations', fontsize=12)
+    axs.set_title('Trips Tracks Lengths\n(Invalid lengths: {0:d}/{1:d}={2:.0f}%)'
+                  .format(invalid, len(all_drives), 100*invalid/len(all_drives)),
+                  fontsize=14)
+    axs.legend()
+    draw()
+
+def to_meters(lat,lon):
+    # Approximations:
+    # earth radius ~ 6400km
+    # Israel latitude ~ 32 degrees
+    # cos(32 degrees) ~ 0.84
+    return ((lat - 32) / 180 * np.pi * 6400000,
+            (lon - 34) / 180 * np.pi * 6400000 * 0.84)
 
 def load_lines(path=r'data/shapes.csv'):
     if not path.endswith('.csv'): path += '.csv'
@@ -46,7 +74,7 @@ def draw():
     plt.get_current_fig_manager().window.showMaximized()
     plt.draw()
     plt.pause(1e-17)
-    plt.tight_layout()
+    plt.tight_layout(3)
 
 def show_lines(lines, drive=None, verbose=0, dynamic=False, grid=True,
                line_nodes=10000, drive_points=10000):
@@ -60,16 +88,19 @@ def show_lines(lines, drive=None, verbose=0, dynamic=False, grid=True,
         x = np.array(x) / 1e3
         y = np.array(y) / 1e3
         ax.plot(np.array(x), np.array(y), colors[i%len(colors)]+'-')
-        ax.plot(x[0], y[0], colors[i%len(colors)]+'o')
-        ax.plot(x[-1], y[-1], colors[i%len(colors)]+'x')
+        ax.plot(x[0], y[0], colors[i%len(colors)]+'o', label='start')
+        if line_nodes==10000:
+            ax.plot(x[-1], y[-1], colors[i%len(colors)]+'x', label='finish')
         if grid:
-            ax.set_title('Shape ID & Route ID: '+l.id, fontsize=10)
+            ax.set_title('Shape ID: {0:s}, Route ID: {1:s}'
+                         .format(l.id.split()[0],l.id.split()[1]), fontsize=10)
             if drive:
                 y,x = zip(*drive.points[:drive_points])
                 x = np.array(x) / 1e3
                 y = np.array(y) / 1e3
-                ax.plot(np.array(x), np.array(y), 'k.')
+                ax.plot(np.array(x), np.array(y), 'k.', label='drive start')
                 ax.plot(x[0], y[0], 'ks')
+        ax.legend()
         if dynamic:
             draw()
             input('press enter...')
@@ -79,6 +110,10 @@ def show_lines(lines, drive=None, verbose=0, dynamic=False, grid=True,
         y = np.array(y) / 1e3
         axs.plot(x, y, 'k.')
         axs.plot(x[0], y[0], 'ks')
+    f.suptitle(('A Single Drive vs. ' if drive else '') +
+               'All Routes in Database' +
+               (' (zoom in beginning of routes)' if line_nodes<10000 else ''),
+               fontsize=14)
     draw()
 
 if __name__ == '__main__':
@@ -88,6 +123,7 @@ if __name__ == '__main__':
         print(e)
     ll = load_lines()
     show_lines(ll)
+    dd = load_drives()
     plt.show()
 
 # interactively:
--- DriveAssigner.py
+++ DriveAssigner.py
@@ -6,18 +6,6 @@ from time import time
 import pickle
 from concurrent.futures import ProcessPoolExecutor
 
-'''
-TODO:
-1. time optimization
-2. improve algo to include directions
-'''
-
-def draw():
-    plt.get_current_fig_manager().window.showMaximized()
-    plt.draw()
-    plt.pause(1e-17)
-    plt.tight_layout()
-
 class BusSystem:
     def __init__(self, lines, path=None):
         self.lines = lines # BusLines
@@ -28,101 +16,158 @@ class BusSystem:
         else:
             self.drives = {}
 
+    def assign_drives(self, drives, save_res=True, distributed=0):
+        if distributed:
+            tpool = ProcessPoolExecutor(max_workers=distributed)
+            ret = tpool.map(self.assign_drive, drives)
+            x = list(ret)
+        else:
+            x = [self.assign_drive(d) for d in drives]
+
+        if save_res:
+            self.save_results(x, [d.id for d in drives])
+
+        return x
+
     def assign_drive(self, drive, save_res=False):
         '''
         drive: a list of tuples (x,y) representing the observed locations.
-        return: line numbers (sorted by probability), corresponding probabilities of bus lines,
-                and plausibility of drive wrt most probable bus line.
+        return: a sorted list of tuples (MSE, 'shape_id route_id').
         '''
         errs = sorted(self.drive_inconsistencies(drive.points))
         if save_res:
             self.save_results([errs], [drive.id])
         return errs
-        #probs,err = self.errors_to_probs(self.drive_inconsistencies(drive))
-        #line_numbers,probs = (list(l) for l in zip(*sorted(zip(self.line_numbers,probs))))
-        # how do I know if it sorts by probs or by line numbers??
-        #return (line_numbers, probs, err)
 
     def save_results(self, errs, ids, save_full_res=True):
         for id,err in zip(ids, errs):
             errs_route_ids = [ id.split(' ')[1] for score,id in err]
+            # filter errors: keep only one Shape ID per each Route ID
             ferrs = [e for i,(e,id) in enumerate(zip(err,errs_route_ids))
                      if id not in set(errs_route_ids[:i]) ]
-            self.drives[id] = {'sid': ferrs[0][1].split()[0], 'rid': ferrs[0][1].split()[1],
-                                     'mse': ferrs[0][0], 'certainty': 1-ferrs[0][0]/ferrs[1][0],
-                                     'res': (err,ferrs) if save_full_res else None}
 
-    def save_to_file(self, path=r'data/res.pkl'):
+            self.drives[id] = {'sid': ferrs[0][1].split()[0], 'rid': ferrs[0][1].split()[1],
+                               'mse': ferrs[0][0], 'certainty': 1-ferrs[0][0]/ferrs[1][0],
+                               'res': (err,ferrs) if save_full_res else None}
+
+    def save_to_file(self, path=r'output/res.pkl'):
         h = open(path, "wb")
         pickle.dump(self.drives, h)
         h.close()
 
-    @staticmethod
-    def errors_to_probs(logq):
-        smallest_inconsistency = np.min(logq)
-        # actually above it's the MSE of the drive wrt closest bus line
-        logq -= np.min(logq)
-        q = np.array([np.exp(lq) if lq>1e-2 else 0 for lq in logq])
-        p = q / np.sum(q)
-        return (p, np.sqrt(smallest_inconsistency))
-
     def drive_inconsistencies(self, drive):
-        return [(bus_line.drive_inconsistency(drive), bus_line.id) for bus_line in self.lines]
+        return [(bus_line.drive_inconsistency(drive), bus_line.id)
+                for bus_line in self.lines]
 
     def print_results(self, only_contradicts=False):
+        # print titles
         print('Trip\tDeclared\tEstimated\tMSE\tCertainty\tContradiction')
+        # print data
         for k in self.drives:
-            if not only_contradicts or k.split()[1].strip() != self.drives[k]['rid'].strip():
+            if not only_contradicts or \
+                            k.split()[1].strip() != self.drives[k]['rid'].strip():
                 print('{0:s}\t{1:s}\t{2:s}\t{3:.0f}\t{4:.2f}\t{5:s}'.format(
                     k.split()[0], k.split()[1], self.drives[k]['rid'],
                     self.drives[k]['mse'], self.drives[k]['certainty'],
-                    '' if k.split()[1].strip() == self.drives[k]['rid'].strip()
-                    else 'CONTRADICTION'))
+                    '' if k.split()[1].strip()==self.drives[k]['rid'].strip()
+                          or k.split()[1].strip()=='0'
+                      else 'CONTRADICTION'))
 
-    def show_drives_errors(self, n=np.inf, vertical_xlabs=True):
+    def show_drives_errors(self, n=80, vertical_xlabs=True):
         n = n if n<=len(self.drives) else len(self.drives)
         f, axs = plt.subplots(2, 1)
+        # 3 best fits (indicating certainty):
         ax = axs[1]
-        ax.bar(tuple(range(n)), [self.drives[k]['res'][1][2][0]
-                                 for k in list(self.drives.keys())[:n]],
-               color='red', label='3rd best')
-        ax.bar(tuple(range(n)), [self.drives[k]['res'][1][1][0]
-                                 for k in list(self.drives.keys())[:n]],
-               color='yellow', label='2nd best')
-        ax.bar(tuple(range(n)), [self.drives[k]['res'][1][0][0]
-                                 for k in list(self.drives.keys())[:n]],
-               color='green', label='best fit')
+        self.drives_errors_barplot(ax, n, 0, 'green',  'best fit')
+        self.drives_errors_barplot(ax, n, 1, 'yellow', '2nd best')
+        self.drives_errors_barplot(ax, n, 2, 'red',    '3rd best')
         ax.set_xlabel('Trip ID')
-        ax.set_ylabel('MSE [m]')
+        ax.set_ylabel('RMSE [m]')
         ax.set_xticks(tuple(range(n)))
-        ax.set_xticklabels(self.drives.keys(), fontsize=14)
+        ax.set_xticklabels([k.split()[0] for k in self.drives.keys()], fontsize=14)
         if vertical_xlabs:
             for tick in ax.get_xticklabels():
                 tick.set_rotation(90)
         ax.legend()
+        # 1 best fit (indicating plausibility):
         ax = axs[0]
-        ax.bar(tuple(range(n)), [self.drives[k]['res'][1][0][0]
-                                 for k in list(self.drives.keys())[:n]],
-               color='green', label='best fit')
+        self.drives_errors_barplot(ax, n, 0, 'green', 'best fit')
         ax.set_xlabel('Trip ID')
-        ax.set_ylabel('MSE [m]')
+        ax.set_ylabel('RMSE [m]')
+        ax.set_title('Errors of Drives with relation to the Likeliest Bus Lines' +
+                     ('' if n>=len(self.drives) else
+                      '\n(first {0:d} drives out of {1:d})'.format(n,len(self.drives))),
+                     fontsize=13)
         ax.set_xticks(tuple(range(n)))
-        ax.set_xticklabels(self.drives.keys(), fontsize=14)
+        ax.set_xticklabels([k.split()[0] for k in self.drives.keys()], fontsize=14)
         if vertical_xlabs:
             for tick in ax.get_xticklabels():
                 tick.set_rotation(90)
         ax.legend()
         draw()
+
+    def drives_errors_barplot(self, ax, n, ith_best, color, label):
+        ax.bar(tuple(range(n)), [self.drives[k]['res'][1][ith_best][0]
+                                 for k in list(self.drives.keys())[:n]],
+               bottom = [self.drives[k]['res'][1][ith_best-1][0] if ith_best else 0
+                         for k in list(self.drives.keys())[:n]],
+               color=color, label=label)
+        ax.set_xlim([-1,n])
 
     def summarize_results(self):
         f, axs = plt.subplots(1, 2)
-        axs[0].plot(sorted([self.drives[k]['mse'] for k in self.drives]))
-        axs[0].set_xlabel('Drive')
-        axs[0].set_ylabel('MSE')
-        axs[1].plot(sorted([self.drives[k]['certainty'] for k in self.drives]))
-        axs[1].set_xlabel('Drive')
-        axs[1].set_ylabel('Certainty')
+        x = 100 * np.array(list(range(len(self.drives)))) / len(self.drives)
+        axs[0].plot(x, sorted([self.drives[k]['mse']
+                               for k in self.drives]))
+        axs[0].set_title('RMSE Distribution Summary', fontsize=14)
+        axs[0].set_xlabel('Drive Percentile [%]', fontsize=12)
+        axs[0].set_ylabel('RMSE', fontsize=12)
+        axs[0].set_xlim([0,100])
+        axs[1].plot(x, sorted([self.drives[k]['certainty']
+                               for k in self.drives]))
+        axs[1].set_title('Certainties Distribution Summary', fontsize=14)
+        axs[1].set_xlabel('Drive Percentile [%]', fontsize=12)
+        axs[1].set_ylabel('Certainty: 1 - RMSE1 / RMSE2', fontsize=12)
+        axs[1].set_xlim([0,100])
         draw()
+
+    def summarize_inconsistencies(self, drives):
+        ids = [k for k in self.drives
+               if k.split()[1].strip() != self.drives[k]['rid'].strip()]
+        print('\nContradictions: {0:d}'.format(len(ids)))
+
+        if ids:
+            drives_orig = self.drives
+            self.drives = {k: self.drives[k] for k in ids}
+
+            self.print_results()
+            self.show_drives_errors()
+            D.show_lines(self.lines, next(d for d in drives if d.id in ids))
+
+            f, axs = plt.subplots(1, 2)
+            x = 100 * np.array(list(range(len(self.drives)))) / len(self.drives)
+            axs[0].plot(x, sorted([len(d.points) for d in drives if d.id in ids]))
+            axs[0].set_title('Trips with Inconsistent Route-ID Assignment',
+                             fontsize=14)
+            axs[0].set_xlabel('Drive Percentile [%]', fontsize=12)
+            axs[0].set_ylabel('Number of GPS observations', fontsize=12)
+
+            mse1 = np.array([self.drives[i]['res'][0][0][0] for i in ids])
+            mse2 = np.array(
+                [next(self.drives[i]['res'][0][j][0] for j in range(len(self.lines))
+                      if self.drives[i]['res'][0][j][1].endswith(i.split()[1]))
+                 for i in ids])
+            axs[1].plot(x, sorted([1-m1/m2 for m1,m2 in zip(mse1,mse2)]))
+            axs[1].set_title('Trips with Inconsistent Route-ID Assignment',
+                             fontsize=14)
+            axs[1].set_xlabel('Drive Percentile [%]', fontsize=12)
+            axs[1].set_ylabel(
+                'Adjusted Certainty: 1 - (best RMSE) / (declared route\'s RMSE)',
+                fontsize=12)
+
+            self.drives = drives_orig
+            draw()
+
 
 class BusLine:
     def __init__(self, id, nodes):
@@ -149,7 +194,7 @@ def norm2(p):
 
 class Interval:
     def __init__(self, a, b):
-        # a=(a1,a2), b=(b1,b2); should represent the line conveniently for the other methods
+        # a=(a1,a2), b=(b1,b2)
         self.a = a
         self.b = b
         self.ba = subtract(b, a)
@@ -166,41 +211,44 @@ class Interval:
         else:
             return norm2(da) - t*t / self.ba_2
 
-if __name__ == '__main__':
+def draw():
+    plt.get_current_fig_manager().window.showMaximized()
+    plt.draw()
+    plt.pause(1e-17)
+    plt.tight_layout()
+
+
+def main(test=False, fetch_only=True, n=3000, res_path='output/res.pkl'):
     t0 = time()
+
     # load data
     ll = D.load_lines()
     print('Number of lines: {0:d}.'.format(len(ll)))
-    dd = D.load_drives()
+    dd = D.load_drives(path='data/test' if test else 'data/train', permit_unlabeled=test)
     print('Number of drive: {0:d}'.format(len(dd)))
     print('Data loaded ({0:.0f} [s]).\n'.format(time()-t0))
+
     # assign drives
-    distributed = True
-    n = 3000
-    print('Drives to assign: {0:d}.'.format(n))
-    b = BusSystem(ll)
-    if distributed:
-        tpool = ProcessPoolExecutor(max_workers=5)
-        ret = tpool.map(b.assign_drive, dd[:n])
-        x = list(ret)
+    if fetch_only:
+        b = BusSystem(ll, res_path)
     else:
-        x = [b.assign_drive(d) for d in dd[:n]]
-    b.save_results(x, [d.id for d in dd[:n]])
-    b.save_to_file()
-    print('Drives assigned ({0:.0f} [s]).\n'.format(time()-t0))
+        print('Drives to assign: {0:.0f}.'.format(np.min([n,len(dd)])))
+        b = BusSystem(ll)
+        b.assign_drives(dd[:n], distributed=5)
+        b.save_to_file(res_path)
+        print('Drives assigned ({0:.0f} [s]).\n'.format(time()-t0))
+
     # show results
     b.show_drives_errors(80)
-    D.show_lines(ll, dd[2])
-    #D.show_lines(ll, dd[2], line_nodes=200, drive_points=7)
+    D.show_lines(ll, dd[0])
+    D.show_lines(ll, dd[0], line_nodes=200, drive_points=7)
     b.summarize_results()
     print('Route IDs Reconstruction:')
     b.print_results()
-    ids = [k for k in b.drives if k.split()[1].strip()!=b.drives[k]['rid'].strip()]
-    print('\nContradictions: {0:d}'.format(len(ids)))
-    if ids:
-        b.print_results(True)
-        b.drives = {k: b.drives[k] for k in ids}
-        b.show_drives_errors()
-        D.show_lines(ll, next(d for d in dd if d.id in ids))
-        # TODO for contradictions - print distribution of lengths, and relative MSEs
+    if not test:
+        b.summarize_inconsistencies(dd)
+
+if __name__ == '__main__':
+    main(test=False, fetch_only=False, n=3000)
+    main(test=True, fetch_only=False, n=3000, res_path='output/test.res.pkl')
     plt.show()
